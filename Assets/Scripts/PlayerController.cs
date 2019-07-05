4cb25fdc40a7c6b7f10b9cf424b3d7cd583787e9
alexnikop <alekos.nik.1995@gmail.com>
2019-07-05 20:03:22 +0300

    Shooter

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private Vector2 moveAmount;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool prevHorStim = false;
+    private bool prevVerStim = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        moveAmount = moveInput.normalized * speed;
+
+        bool horStim = Mathf.Abs(moveInput[0]) > 0.5;
+        bool verStim = Mathf.Abs(moveInput[1]) > 0.5;
+
+        if (horStim && !prevHorStim){
+            anim.SetBool("isWalkingHor", true);
+            anim.SetBool("isWalkingVer", false);
+        }
+        if (verStim && !prevVerStim){
+            anim.SetBool("isWalkingVer", true);
+            anim.SetBool("isWalkingHor", false);    
+        }
+        if (!horStim){
+            anim.SetBool("isWalkingHor", false);
+        }
+        if (!verStim){
+            anim.SetBool("isWalkingVer", false);        
+        }
+        
+        prevHorStim = horStim;
+        prevVerStim = verStim;
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);    
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    public float speed;
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+}
